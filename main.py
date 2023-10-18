060bba458008a285750de12ef131a8110884395f
Akshay Lokur <alokur@apple.com>
2023-10-18 15:28:07 +0800

    Animate and keybinding

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from turtle import Screen
 from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game :)")
+screen.tracer(0)  # to disable automatic screen tracing / refresh / animation
 
 snake = Snake(snake_body_length=3)
+screen.update()
+
+screen.listen()  # listen to keystrokes
+# Bind Arrow keys
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,16 +1,52 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+LEFT = 180
+RIGHT = 0
+UP = 90
+DOWN = 270
+
 
 class Snake:
-
     def __init__(self, snake_body_length):
+        self.body = []
         self.snake_body_length = snake_body_length
-        self.make_snake()
+        self.make()
+        self.head = self.body[0]
 
-    def make_snake(self):
+    def make(self):
         x_pos = 0
         for i in range(0, self.snake_body_length):
             t = Turtle(shape="square")
             t.color("white")
+            t.penup()
             t.goto(x_pos, y=0)
             x_pos -= 20
+            self.body.append(t)
+
+    def move(self):
+        for part in range(len(self.body) - 1, 0, -1):
+            new_x = self.body[part - 1].xcor()
+            new_y = self.body[part - 1].ycor()
+            self.body[part].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            print("moving up")
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != UP:
+            print("moving down")
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            print("moving left")
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            print("moving right")
+            self.head.setheading(0)
