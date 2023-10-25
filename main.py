f5a66a36de6c1f1d1758be67586970c8837ffd12
Akshay Lokur <alokur@apple.com>
2023-10-25 14:01:06 +0800

    Detect collision with walls.

--- main.py
+++ main.py
@@ -32,4 +32,9 @@ while is_game_on:
         food.refresh()
         scoreboard.update_scoreboard()
 
+    # Collision with the wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        is_game_on = False
+
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -2,15 +2,24 @@ from turtle import Turtle
 
 
 class ScoreBoard(Turtle):
+    TEXT_ALIGNMENT = "center"
+    FONT = ('Courier New', 16, 'normal')
+
     def __init__(self):
         super().__init__()
         self.score = 0
         self.hideturtle()
+        self.pu()
         self.goto(0, 275)
+        self.color("white")
 
     def update_scoreboard(self):
         self.score += 1
         self.clear()
-        self.color("white")
         print(f"Score is: {self.score}")
-        self.write(f"Score = {self.score}", False, align="center", font=('Arial', 16, 'normal'))
+        self.write(f"Score = {self.score}", False, align=ScoreBoard.TEXT_ALIGNMENT, font=ScoreBoard.FONT)
+
+    def game_over(self):
+        print("GAME OVER!")
+        self.goto(0, 0)
+        self.write("GAME OVER", False, align=ScoreBoard.TEXT_ALIGNMENT, font=ScoreBoard.FONT)
